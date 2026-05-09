0136d38efe5f79ab88b311f8556afd79b0001d64
JydnMrsh <86445017+JydnMrsh@users.noreply.github.com>
2026-05-10 08:20:11 +1200

    Implemented Skeleton Project for Phase 2 Refactoring

---
+++ Phase2 Implementation/Phase-2-Warehouse-Management-System/Phase-2-Warehouse-Management-System/Models/DesignPatterns/ObserverPattern.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase_2_Warehouse_Management_System.DesignPatterns
+{
+    internal class ObserverPattern
+    {
+    }
+}
---
+++ Phase2 Implementation/Phase-2-Warehouse-Management-System/Phase-2-Warehouse-Management-System/Models/Interfaces.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase_2_Warehouse_Management_System.Models
+{
+    internal class Interfaces
+    {
+    }
+}
