481a39a34c83da900ec04e4a8301d29f54698d2c
JydnMrsh <86445017+JydnMrsh@users.noreply.github.com>
2026-05-11 11:24:24 +1200

    Created RetailStaff skeleton class
    Should be fully implemented

--- Implementation/BunningsInventoryManagementSystem/Models/RetailStaff.cs
+++ Implementation/BunningsInventoryManagementSystem/Models/RetailStaff.cs
@@ -8,5 +8,26 @@ namespace BunningsInventoryManagementSystem.Models
 {
     internal class RetailStaff : User
     {
+        // Retail staff can update stock levels
+        public void UpdateStockLevel(Item item, int amount)
+        {
+            item.UpdateStockLevel(amount);
+        }
+
+        // Retail staff can transfer stock to warehouse
+        public void TransferStockToWarehouse(Item item, bool newLocation)
+        {
+            item.UpdateLocation(newLocation);
+        }
+
+        // Retail staff can view stock levels and item locations
+        public int ViewStockLevel(Item item)
+        {
+            return item.stockLevel;
+        }
+        public bool ViewItemLocation(Item item)
+        {
+            return item.location;
+        }
     }
 }
