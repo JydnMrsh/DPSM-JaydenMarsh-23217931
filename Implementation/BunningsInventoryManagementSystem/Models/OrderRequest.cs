4af042a32487e993db1acbc3e4f804cba8f058ef
JydnMrsh <86445017+JydnMrsh@users.noreply.github.com>
2026-05-11 11:58:21 +1200

    Added OrderRequest skeleton class
    with some implementation

--- Implementation/BunningsInventoryManagementSystem/Models/OrderRequest.cs
+++ Implementation/BunningsInventoryManagementSystem/Models/OrderRequest.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace BunningsInventoryManagementSystem.Models
 {
+    public enum Status
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+
     internal class OrderRequest
     {
+        public int RequestID { get; set; }
+        public Status Status { get; set; }
+        public Item RequestedItem { get; set; }
+        public int Amount { get; set; }
+
+
+        public OrderRequest()
+        {
+            Status = Status.Pending; // Default status is pending
+        }
+
+        // Set the status of the order request to approved or rejected
+        public void Approve()
+        {
+            Status = Status.Approved;
+        }
+        public void Reject()
+        {
+            Status = Status.Rejected;
+        }
     }
 }
