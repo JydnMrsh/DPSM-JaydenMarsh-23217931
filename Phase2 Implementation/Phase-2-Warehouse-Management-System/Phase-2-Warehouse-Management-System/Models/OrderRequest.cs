f7b91c58e7a7b21f34e073a40f8f82e11788abbc
JydnMrsh <86445017+JydnMrsh@users.noreply.github.com>
2026-05-18 22:02:07 +1200

    Implemented Order Request class

--- Phase2 Implementation/Phase-2-Warehouse-Management-System/Phase-2-Warehouse-Management-System/Models/OrderRequest.cs
+++ Phase2 Implementation/Phase-2-Warehouse-Management-System/Phase-2-Warehouse-Management-System/Models/OrderRequest.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Phase_2_Warehouse_Management_System.Models; // Access Models
 
 namespace Phase_2_Warehouse_Management_System.Models
 {
-    internal class OrderRequest
+    public enum OrderStatus { Pending, Approved, Rejected } // Order status's
+
+    public class OrderRequest
     {
+        public int RequestId { get; private set; }
+        public int ItemId { get; private set; }
+        public string ItemName { get; private set; }
+        public int Quantity { get; private set; }
+        public OrderStatus Status { get; private set; }
+        public string RequestedBy { get; private set; } // Which manager requested it now that we have implementation for multiple managers.
+
+        // Order request constructor
+        public OrderRequest(int requestId, int itemId, string itemName, int quantity, string requestedBy)
+        {
+            RequestId = requestId;
+            ItemId = itemId;
+            ItemName = itemName;
+            Quantity = quantity;
+            Status = OrderStatus.Pending;
+            RequestedBy = requestedBy;
+        }
     }
 }
